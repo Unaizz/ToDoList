9e02e49628b02a947c1e3e2761bc9af791eb6d7b
Unaiz Siddiqui <unaiz4321@gmail.com>
2023-06-13 23:33:26 +0530

    number added

--- ToDoList/ToDoList/Program.cs
+++ ToDoList/ToDoList/Program.cs
@@ -1,4 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Unaiz");
-Console.WriteLine("Unaiz");
+Console.WriteLine("Trying");
+var arr = new[] { 1, 2, 3, 4, 5, 5, 6, 8 };
+foreach(var number in arr)
+{
+    Console.WriteLine(number);
+}
 Console.ReadKey();
